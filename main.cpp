998ea2bb95a38bdecbf89eff41bfdf98a452c35a
Akantonio <adrianvantonio@yahoo.com>
2021-04-28 08:31:26 -0800

    added stopwatch

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "StopWatch.h"
 int fib(int n){
     if(n==0||n==1){
         return n;
@@ -30,18 +30,34 @@ int ack(int m,int n){
     }
 }
 
+void timingAckerman(int m, int n,StopWatch & time){
+    std::cout<<"The values before it crashes m: "<<m<<" The n: "<< n << std::endl;
+    time.start();
+    std::cout << ack(m, n) << std::endl;
+    time.stop();
+    time.readTime(1);
+}
 int main() {
-    int n=9;
+    int n =0;
     std::cout << "Recursion" << std::endl;
-    while(n!=0) {
-        std::cout << fib(n)<<" ";
-        n--;
+    while (n < 10) {
+        std::cout << fib(n) << " ";
+        n++;
     }
-    std::cout<<"\n";
+    std::cout << "\n";
     std::cout << "Fib_loop" << std::endl;
     fib_loop(9);
+    std::cout << "\n";
 
-    std::cout<<"Ackerman's"<<std::endl;
-    std::cout<< ack(,0);
+    std::cout << "Ackerman's function with integers" << std::endl;
+    //Crashes when the number is higher than 2^32
+    //  which is the highest amount that an Integer can store.
+    //Crashes at ack(4,n>0)
+    //Crashes at ack(3,n>12)
+    //Crashes at ack(2,n>21666)
+    StopWatch time;
+    timingAckerman(4,0,time);
+    timingAckerman(3,12,time);
+    timingAckerman(2,21666,time);
     return 0;
 }
